88a73dae2e0729b649ce40e59f9142b5c75f5cc1
jimmybot <jimmybot@jimmybot.com>
2025-02-07 12:42:36 -0500

    Upgrade dependencies to latest

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule RedixPool.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-     {:redix, "~> 0.9"},
+     {:redix, "~> 1.5"},
      {:poolboy, "~> 1.5"}]
   end
 end
